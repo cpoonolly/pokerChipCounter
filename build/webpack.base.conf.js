bb0d38df074d4deb7367ae324eedff40218ea49e
Cherry Poonolly <rpoonolly@gmail.com>
2018-12-18 17:03:53 -0500

    FEAT - Adding css normalization

--- build/webpack.base.conf.js
+++ build/webpack.base.conf.js
@@ -74,7 +74,15 @@ module.exports = {
           limit: 10000,
           name: utils.assetsPath('fonts/[name].[hash:7].[ext]')
         }
-      }
+      },
+      {
+        test: /\.(css)$/,
+        use: [{
+            loader: "style-loader" // creates style nodes from JS strings
+        }, {
+            loader: "css-loader" // translates CSS into CommonJS
+        }]
+    }
     ]
   },
   node: {
