84b344a65ec167d82d89975e9ebddfe9421c2c57
Cherry Poonolly <rpoonolly@gmail.com>
2018-12-18 18:02:54 -0500

    FEAT - Small changes to StartScreen Component

--- src/components/StartScreen.vue
+++ src/components/StartScreen.vue
@@ -1,7 +1,21 @@
 <template>
   <div class="start-screen">
-    <img class="game-logo" src="../assets/pokerchip.png">
-    <h1 class="game-title">{{gameTitle}}</h1>
+    <div class="row">
+      <h1 class="game-title">{{gameTitle}}</h1>
+    </div>
+    <div class="row">
+      <img class="game-logo" src="../assets/pokerchip.png">
+    </div>
+    <div class="row">
+      <a class="new-game-btn btn-large waves-effect waves-light">
+        New Game
+      </a>
+    </div>
+    <div class="row" v-if="false">
+      <a class="new-game-btn btn-large waves-effect waves-light">
+        Continue
+      </a>
+    </div>
   </div>
 </template>
 
@@ -14,12 +28,26 @@ export default {
 </script>
 
 <style scoped>
-.game-logo {
-  max-height: 512px;
-  max-width: 512px;
+@font-face {
+  font-family: 'Borgens Burlesque';
+  src: url('../assets/borgens_burlesque.otf');
 }
 
 .game-title {
-    color: green;
+  font-family: 'Borgens Burlesque'
 }
+
+.game-logo {
+  max-height: 512px;
+  max-width: 512px;
+
+  -webkit-animation: spin 10s linear infinite;
+  -moz-animation: spin 10s linear infinite;
+  animation: spin 10s linear infinite;
+}
+
+@-moz-keyframes spin { 100% { -moz-transform: rotate(360deg); } }
+@-webkit-keyframes spin { 100% { -webkit-transform: rotate(360deg); } }
+@keyframes spin { 100% { -webkit-transform: rotate(360deg); transform:rotate(360deg); } }
+
 </style>
